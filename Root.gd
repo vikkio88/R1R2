318017dd0f588c6bc052e40821bfb23a4cbc1f7c
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-08-25 21:26:28 +0100

    adding screen shake effect

--- Root.gd
+++ Root.gd
@@ -13,6 +13,17 @@ const buttonsClickedDefault = {
 	LT = false
 }
 
+
+onready var Cam = $Cam;
+onready var noise = OpenSimplexNoise.new()
+var noise_y = 0
+var trauma = 0.0  # Current shake strength.
+var trauma_power = 2  # Trauma exponent. Use [2, 3].
+export var decay = 0.8  # How quickly the shaking stops [0, 1].
+export var max_offset = Vector2(100, 75)  # Maximum hor/ver shake in pixels.
+export var max_roll = 0.1  # Maximum rotation in radians (use sparingly)
+
+
 onready var SpriteNodes = {
 	Y = $InputView/Active/Y,
 	X = $InputView/Active/X,
@@ -34,16 +45,27 @@ onready var GameSpriteNodes = {
 	LB = $GameView/LB,
 	LT = $GameView/LT
 }
+var buttonClicked = buttonsClickedDefault
 
 func _init():
 	is_linux = not ("Window" in OS.get_name())
+
+func _ready():
+	randomize()
+	noise.seed = randi()
+	noise.period = 4
+	noise.octaves = 2
 
 #func _input(event):
 #	if event is InputEventJoypadMotion:
 #		print_debug(event.as_text())
 
-
-var buttonClicked = buttonsClickedDefault
+func shake():
+	var amount = pow(trauma, trauma_power)
+	noise_y += 1
+	Cam.rotation = max_roll * amount * noise.get_noise_2d(noise.seed, noise_y)
+	Cam.offset.x = max_offset.x * amount * noise.get_noise_2d(noise.seed*2, noise_y)
+	Cam.offset.y = max_offset.y * amount * noise.get_noise_2d(noise.seed*3, noise_y)
 
 func _process(delta):
 	if Input.is_action_just_released("Y") or Input.is_action_pressed("Y"):
@@ -75,6 +97,13 @@ func _process(delta):
 	for key in buttonClicked:
 		if key in SpriteNodes:
 			SpriteNodes[key].visible = buttonClicked[key]
+			
+	if Input.is_action_just_released("ui_accept"):
+		trauma = .5
+	
+	if trauma:
+		trauma = max(trauma - decay * delta, 0)
+		shake()
 	
 	buttonClicked = buttonsClickedDefault.duplicate()
 	
